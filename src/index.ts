08f22d80dbb031ae1591b0d4ab0423695c50319c
AnnaWillis <arw2451@gmail.com>
2023-06-16 22:54:03 -0400

    made the project work with gcloud :)

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,8 @@ app.use(
   }
 );
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
